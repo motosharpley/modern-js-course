76de2e8f1b0f07d88c50d227418045cc36c2a304
Bryan Sharpley <motosharpley@gmail.com>
2018-04-26 23:46:27 -0500

    var and naming convention

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 // alert('Hello Javascript!');
 console.log('I am a conslole log');
 var myVariable = 'Hi vars!';
@@ -17,3 +18,30 @@ console.time("That's all folks!");
   console.log('I need more time!');
   console.log('I need more time!');
 console.timeEnd("That's all folks!");
+*/
+
+// JS Variables var, let, const
+
+// declare a variable 
+var myVariableName = 'My Variable Value';
+console.log(myVariableName);
+// reassign the variable value
+myVariableName = 'A New Assigned Value';
+console.log(myVariableName);
+
+// Initialize a var
+var myCoolVar
+console.log(myCoolVar); // should return undefined
+// assing a value to a var
+myCoolVar = 'I am not undefined anymore wohoo!';
+console.log(myCoolVar);
+
+// vars can only contain letters, numbers, _, $
+// Can not start with a number
+
+// naming conventions 
+var camelCase = 'camelCaseLooksLikeThis';
+var under_score = 'with_under_score_you_connect_like_this';
+var PascalCase = 'WithPascalEveryFirstLetterIsCapital';
+// Constants should be UPPER_CASE
+var CONSTANT_VAR = 'this naming convention helps identify constants in js';
