d6dfc3ec5663e1bf78388eaa2594731fa5699589
Bryan Sharpley <motosharpley@gmail.com>
2018-05-07 16:08:25 -0500

    switch statements

--- conditionals.js
+++ conditionals.js
@@ -81,3 +81,43 @@ if(age < 18 || age > 66) {
 
 // Ternary Operator
 console.log(id === 100 ? true : false);
+
+// Switches
+
+switch(color) {
+  case 'red' :
+  console.log('Color is red');
+  break;
+  case 'blue':
+  console.log('Color is blue');
+  break;
+  default:
+  console.log('Color is not red or blue');
+  break;
+}
+
+switch(new Date().getDay()){
+  case 0 :
+    day = 'Sunday';
+    break;
+  case 1 :
+    day = 'Monday';
+    break;
+  case 2 :
+    day = 'Tuesday';
+    break;
+  case 3 :
+    day = 'Wednesday';
+    break;
+  case 4 :
+    day = 'Thursday';
+    break;
+  case 5 :
+    day = 'Friday';
+    break;
+  case 6 :
+    day = 'Saturday';
+    break;
+}
+
+console.log(`Today is ${day}`);
