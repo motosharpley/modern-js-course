dad25f89023fbc9b8ddc2f3762572fc345207ec2
Bryan Sharpley <motosharpley@gmail.com>
2018-05-14 22:19:58 -0500

    event bubbling and delegation

--- events.js
+++ events.js
@@ -95,3 +95,38 @@
 //   // console.log(taskInput.value);
 //   // e.preventDefault();
 // }
+
+
+// EVENT BUBBLING
+
+// document.querySelector('.card-title').addEventListener('click', function(){
+//   console.log('card-title');
+// })
+
+// document.querySelector('.card-content').addEventListener('click', function(){
+//   console.log('card content');
+// })
+
+// document.querySelector('.card').addEventListener('click', function(){
+//   console.log('card');
+// })
+
+// EVENT DELEGATION
+
+/* --- without delegation ---
+const delItem = document.querySelector('.delete-item');
+delItem.addEventListener('click', deleteItem);
+*/
+
+// document.body.addEventListener('click', deleteItem);
+
+// function deleteItem(){
+//   // console.log(e.target);
+//   // if(e.target.parentElement.className === 'delete-item secondary-content'){
+//   //   console.log('delete item');
+//   // }
+//   if(e.target.parentElement.classList.contains('delete-item')){
+//     console.log('delete item');
+//     e.target.parentElement.parentElement.remove();
+//   }
+// }
